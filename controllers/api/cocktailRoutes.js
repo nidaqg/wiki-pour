6ad3ca439755ffc9f7bc58d6fd5136b15e0807e9
Gary B <grider27@gmail.com>
2021-04-03 09:42:28 -0400

    updated cockatil routes for api to allow for all associations to be passed for new creation

--- controllers/api/cocktailRoutes.js
+++ controllers/api/cocktailRoutes.js
@@ -1,19 +1,66 @@
 const router = require('express').Router();
-const { Cocktail } = require('../../models');
+const { Cocktail, CocktailIngredient, CocktailCategoryType } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// router.post('/', withAuth, async (req, res) => {
+//     try {
+//         const newCocktail = await Cocktail.create({
+//             ...req.body,
+//             user_id: req.session.user_id,
+//         });
+
+//         res.status(200).json(newCocktail);
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
+
+// new cocktail with all associations.
+router.post('/', async (req, res) => {
+    /* expected req.body:
+        {
+        cocktail_name: "cocktail name",
+        instructions: "do this",
+        user_id: 1,
+        ingredientIds: [1, 2],
+        categoryTypeIds: [1, 2]
+        }
+    */
     try {
         const newCocktail = await Cocktail.create({
             ...req.body,
             user_id: req.session.user_id,
         });
 
+        if (req.body.ingredientIds.length) {
+            const cocktailIngredientIdArr = req.body.ingredientIds.map((ingredient_id) => {
+                return {
+                    cocktail_id: newCocktail.id,
+                    ingredient_id,
+                };
+            });
+            const newCocktailIngredient = await CocktailIngredient.bulkCreate(cocktailIngredientIdArr);
+        }
+
+
+        if (req.body.categoryTypeIds.length) {
+            const cocktailCategoryTypesIdArr = req.body.categoryTypeIds.map((categoryType_id) => {
+                return {
+                    cocktail_id: newCocktail.id,
+                    categoryType_id,
+                };
+            });
+            const newCocktailCategoryType = await CocktailCategoryType.bulkCreate(cocktailCategoryTypesIdArr);
+        }
+
+
         res.status(200).json(newCocktail);
     } catch (err) {
         res.status(400).json(err);
     }
+
 });
+
 
 router.put('/edit/:id', withAuth, async (req, res) => {
     try {

a4f75e764c20efef25e8083d9994f2d0bdcf18fc
Gary B <grider27@gmail.com>
2021-04-03 12:25:47 -0400

    dynamically get list data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const cocktailRoutes = require('./cocktailRoutes');
 const ratingRoutes = require('./ratingRoutes');
 const ingredientRoutes = require('./ingredientRoutes');
 const categoryTypeRoutes = require('./categoryTypeRoutes');
+const brandRoutes = require('./brandRoutes');
 
 router.use('/users', userRoutes);
 router.use('/cocktail', cocktailRoutes);
 router.use('/rating', ratingRoutes);
 router.use('/ingredient', ingredientRoutes);
 router.use('/categorytype', categoryTypeRoutes);
+router.use('/brand', brandRoutes);
 
 module.exports = router;
